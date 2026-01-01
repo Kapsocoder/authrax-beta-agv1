fa6ba6b1c03805898ee4fa11fe91b678d1db80f1
Kapil <kapil_sabharwal@yahoo.com>
2026-01-02 00:39:26 +1100

    chore: Remove temporary debug files from repo

--- functions/start_debug.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d C:\Users\kapil\OneDrive\Business\Development\Authrax-Beta-Lv1\authrax\functions
-echo Starting Emulator with logging... > debug_output.txt
-npm.cmd run serve >> debug_output.txt 2>&1
